3ed46978fb87f1a322f11812a7078b2bdca6e299
j018igam <r.igami2475@gmail.com>
2024-06-26 02:46:37 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
-
-int myRoot(double x) // x の平方根を返す関数。x >= 0 ではないときエラー表示を行い exit(1) とする。 
+#include <stdlib.h> //exit()関数を使うために追加
+#include <math.h> //sqrt()関数を使うために追加
+double myRoot(double x) // x の平方根を返す関数。x >= 0 ではないときエラー表示を行い exit(1) とする。またdouble型を返すためmyRootはdouble型にする。 
 {
-  int y;
-  if( x <= 0 ){
+  double y; //sqrt()はdouble型で返すのでyもdouble型である
+  if( x < 0 ){ //x=0のときはエラーに含めない
     printf("エラー：0以上の値ではありません in myRoot()\n");　      
     exit(1);
   }
   y = sqrt(x);
   return y;
 }
-
 int main(void)
 {
-  int i;
+  int i=0; //iが初期化されていない
   double *x = (double *)malloc(sizeof(double) * 100);
 
   while( i <= 100 ){
     x[i] = myRoot((double)i);
     i++;
   }
-
   //これ以降は確認用コードなので、修正しないこと
   printf("x[0]=%f\n", x[0]);
   printf("x[1]=%f\n", x[1]);
